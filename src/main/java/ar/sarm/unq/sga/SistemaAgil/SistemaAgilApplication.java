946f31ae119948cd88c53db47f2d25c84557c605
valeria <valeriagrandi@hotmail.com>
2018-10-08 19:43:02 -0300

    gitignore, mvn,bin,mvnw,mvnw.cmd,pon.xml,src

---
+++ src/main/java/ar/sarm/unq/sga/SistemaAgil/SistemaAgilApplication.java
@@ -0,0 +1,12 @@
+package ar.sarm.unq.sga.SistemaAgil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAgilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAgilApplication.class, args);
+	}
+}
---
+++ src/test/java/ar/sarm/unq/sga/SistemaAgil/SistemaAgilApplicationTests.java
@@ -0,0 +1,16 @@
+package ar.sarm.unq.sga.SistemaAgil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SistemaAgilApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
